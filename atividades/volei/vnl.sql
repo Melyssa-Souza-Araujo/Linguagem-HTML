2b15abd40a0488e5b16a7d84a68f06903cd70f68
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2026-06-11 12:05:42 -0300

    Update vnl.sql

--- atividades/volei/vnl.sql
+++ atividades/volei/vnl.sql
@@ -1,21 +1,22 @@
 CREATE DATABASE vnl_sistema;
 USE vnl_sistema;
 
+-- Criação da tabela de Países
 CREATE TABLE paises (
-    id INT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
     bandeira_url VARCHAR(255) DEFAULT NULL
 );
 
+-- Criação da tabela de Partidas JÁ COM A COLUNA GENERO integrada
 CREATE TABLE partidas (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     id_casa INT NOT NULL,
     id_fora INT NOT NULL,
     pontos_casa INT NOT NULL DEFAULT 0,
     pontos_fora INT NOT NULL DEFAULT 0,
     youtube_url VARCHAR(255) DEFAULT NULL,
+    genero ENUM('M', 'F') NOT NULL DEFAULT 'F',
     FOREIGN KEY (id_casa) REFERENCES paises(id),
     FOREIGN KEY (id_fora) REFERENCES paises(id)
 );
-
-ALTER TABLE partidas ADD COLUMN genero ENUM('M', 'F') NOT NULL DEFAULT 'F' AFTER youtube_url;
