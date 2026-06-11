16ff33474ab9933c5dcfc06de4e39089e990dd0f
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2026-06-11 10:08:18 -0300

    Add files via upload

---
+++ atividades/volei/vnl.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE vnl_sistema;
+USE vnl_sistema;
+
+CREATE TABLE paises (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    bandeira_url VARCHAR(255) DEFAULT NULL
+);
+
+CREATE TABLE partidas (
+    id INT PRIMARY KEY,
+    id_casa INT NOT NULL,
+    id_fora INT NOT NULL,
+    pontos_casa INT NOT NULL DEFAULT 0,
+    pontos_fora INT NOT NULL DEFAULT 0,
+    youtube_url VARCHAR(255) DEFAULT NULL,
+    FOREIGN KEY (id_casa) REFERENCES paises(id),
+    FOREIGN KEY (id_fora) REFERENCES paises(id)
+);
